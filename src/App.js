7a973eb14be474612eda597f3b81cef4b964bed6
Anna Fulton <afulton7@gmail.com>
2019-06-20 11:45:41 -0400

    009-prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-// 
-
-// create Pet component 
-const Pet = ({name, animal, breed}) => { // destructuring "props"
-    return React.createElement("div", {}, [ // create array of elements
-        React.createElement("h1", {}, name), 
-        React.createElement("h2", {}, animal), 
-        React.createElement("h2", {}, breed)
-    ])
+// create Pet component
+const Pet = ({ name, animal, breed }) => {
+  // destructuring "props"
+  return React.createElement("div", {}, [
+    // create array of elements
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ])
 }
 
 // create App component
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {name: "Henry", animal: "Dog", breed: "Weimaraner"}),
-        React.createElement(Pet, {name: "Bear", animal: "Dog", breed: "English Shepherd"}),
-        React.createElement(Pet, {name: "Piper", animal: "Dog", breed: "English Shepherd"}),
-        React.createElement(Pet, {name: "Jack", animal: "Dog", breed: "Dachshund"})
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Henry",
+      animal: "Dog",
+      breed: "Weimaraner"
+    }),
+    React.createElement(Pet, {
+      name: "Bear",
+      animal: "Dog",
+      breed: "English Shepherd"
+    }),
+    React.createElement(Pet, {
+      name: "Piper Lou",
+      animal: "Dog",
+      breed: "English Shepherd"
+    }),
+    React.createElement(Pet, {
+      name: "Jack",
+      animal: "Dog",
+      breed: "Dachshund"
+    })
+  ])
 }
 
 // render the App component to the DOM
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
